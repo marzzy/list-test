599c032aeaf6b40f9e85109f61c8e2284ed7533f
marzzy mogh <marzzy.m@gmail.com>
2020-04-24 19:08:11 +0430

    chore: remove unused codes

--- src/App/Hooks/DataHook.js
+++ src/App/Hooks/DataHook.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import DataReducer from '../Reducers/DataReducer';
 import { setQueryParams,
   findQueryParamsWithKey,
